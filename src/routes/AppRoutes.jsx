48da524b88a92f3c1a6a4e7319f5a7e8a47e99ad
argelhr <argelhr@hotmail.com>
2024-11-20 14:57:18 -0300

    feat: novas rotas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -6,7 +6,11 @@ const AppRoutes = () => {
   return (
     <Router>
       <Routes>
-        <Route path={routes.home} element={<Header />}></Route>
+        <Route path={routes.home} element={<Header />}>
+          <Route path={routes.donate_books} element={<h1>Livros doados</h1>} />
+          <Route path={routes.donate} element={<h1>Quero doar</h1>} />
+        </Route>
+        <Route path="*" element={<h1>404</h1>} />
       </Routes>
     </Router>
   );
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,7 @@
 const routes = {
   home: "/",
+  donate_books: "/livros_doados",
+  donate: "/quero_doar",
 }
 
 export default routes;
