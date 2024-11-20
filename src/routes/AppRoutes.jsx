ab7336ac09b0223627e67a3199b1ccb31c696361
argelhr <argelhr@hotmail.com>
2024-11-20 15:31:46 -0300

    fix: arrumando rotas aninhadas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "../components/Header";
+import PageLayout from "../pages/PageLayout";
 import routes from "./routes";
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
-        <Route path={routes.home} element={<Header />}>
+        <Route path={routes.home} element={<PageLayout />}>
+          <Route index element={<h1>Home</h1>} />
           <Route path={routes.donate_books} element={<h1>Livros doados</h1>} />
           <Route path={routes.donate} element={<h1>Quero doar</h1>} />
         </Route>
