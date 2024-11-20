346b583c5442909dc475c10b15fbe1b7d2b10678
argelhr <argelhr@hotmail.com>
2024-11-20 14:09:19 -0300

    feat: inicio da header prev styles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,27 @@
+import logo from "../../assets/logo.png";
+import lupa from "../../assets/search.png";
+import style from "./styles.module.scss";
+
 const Header = () => {
   return (
     <header>
-      <h1>header</h1>
+      <section>
+        <figure>
+          <img src={logo} alt={"logotipo - livros vai na web"} />
+        </figure>
+        <h1>Livros Vai na Web</h1>
+      </section>
+      <nav>
+        <ul>
+          <li>Início</li>
+          <li>Livros Doados</li>
+          <li>Quero doar</li>
+        </ul>
+      </nav>
+      <form onSubmit={(e) => e.preventDefault} action="#">
+        <input type="text" name="search" />
+        <img src={lupa} alt={`ícone de lupa "o que você procura?"`} />
+      </form>
     </header>
   );
 };
