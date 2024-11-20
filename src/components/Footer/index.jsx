6c6203dd955cc33ad6861a63cbd2bce25fe43a6c
argelhr <argelhr@hotmail.com>
2024-11-20 17:49:41 -0300

    feat: finalizando footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,49 @@
 import React from "react";
-import style from "./styles.module.css";
+import style from "./styles.module.scss";
+import facebook from "../../assets/facebook.png";
+import x from "../../assets/x.png";
+import youtube from "../../assets/youtube.png";
+import linkedin from "../../assets/linkedin.png";
+import instagram from "../../assets/instagram.png";
 
 const Footer = () => {
   return (
     <footer>
-      <p>footer</p>
+      <div className={style.footer_container}>
+        <a href="https://www.youtube.com/watch?v=JQuqkk7deOc" target="_blank" rel="noopener noreferrer" aria-label="Ligar para o número 4 0 0 2 2 8 9 2 2 e pedir playstation, playstation, playstation">
+          4002-8922
+        </a>
+        <aside>
+          <ul>
+            <li>
+              <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Link para a página do Facebook">
+                <img src={facebook} alt="Logotipo do Facebook" />
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" aria-label="Link para a página do X">
+                <img src={x} alt="Logotipo do Twitter" />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer" aria-label="Link para a página do YouTube">
+                <img src={youtube} alt="Logotipo do YouTube" />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" aria-label="Link para a página do LinkedIn">
+                <img src={linkedin} alt="Logotipo do LinkedIn" />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" aria-label="Link para a página do Instagram">
+                <img src={instagram} alt="Logotipo do Instagram" />
+              </a>
+            </li>
+          </ul>
+        </aside>
+      </div>
+      <div className={style.footer_container}></div>
     </footer>
   );
 };
