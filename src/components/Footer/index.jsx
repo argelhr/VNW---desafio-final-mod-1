f807722fbd2d5a558e616b22887c40ea91d190bb
argelhr <argelhr@hotmail.com>
2024-11-20 16:41:22 -0300

    feat: adiciona componente Footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./styles.module.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>footer</p>
+    </footer>
+  );
+};
+
+export default Footer;
