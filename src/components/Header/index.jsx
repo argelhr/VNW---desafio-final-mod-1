c5c42a8a6c38cb464b47d54c71378e679951557f
argelhr <argelhr@hotmail.com>
2024-11-20 14:57:33 -0300

    feat: nova header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,27 +1,50 @@
+import { useRef } from "react";
+import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import lupa from "../../assets/search.png";
 import style from "./styles.module.scss";
 
 const Header = () => {
+  const searchInput = useRef(null);
+
+  const onFormClick = () => {
+    searchInput.current.focus();
+  };
+
   return (
     <header>
-      <section>
-        <figure>
-          <img src={logo} alt={"logotipo - livros vai na web"} />
-        </figure>
-        <h1>Livros Vai na Web</h1>
-      </section>
-      <nav>
-        <ul>
-          <li>Início</li>
-          <li>Livros Doados</li>
-          <li>Quero doar</li>
-        </ul>
-      </nav>
-      <form onSubmit={(e) => e.preventDefault} action="#">
-        <input type="text" name="search" />
-        <img src={lupa} alt={`ícone de lupa "o que você procura?"`} />
-      </form>
+      <div className={style.container_header}>
+        <section>
+          <Link to="/">
+            <figure>
+              <img src={logo} alt={"logotipo - livros vai na web"} />
+            </figure>
+            <h1>
+              <span>Livros</span>Vai na Web
+            </h1>
+          </Link>
+        </section>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Início</Link>
+            </li>
+            <li>
+              {" "}
+              <Link to="/livros_doados">Livro Doados</Link>{" "}
+            </li>
+            <li>
+              <Link to="/quero_doar">Quero doar</Link>
+            </li>
+          </ul>
+        </nav>
+        <form onSubmit={(e) => e.preventDefault()} action="#" onClick={onFormClick}>
+          <input ref={searchInput} type="search" name="search" placeholder="O que você procura?" />
+          <button type="submit">
+            <img src={lupa} alt={`ícone de lupa "o que você procura?"`} />
+          </button>
+        </form>
+      </div>
     </header>
   );
 };
